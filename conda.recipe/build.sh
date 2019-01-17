c2b807e4a44d932a8951c65ab619d7f1547404d2
Alex Liberzon <alex.liberzon@gmail.com>
2019-01-17 22:15:04 +0200

    updated pyptv with a new conda recip using other conda packages
    like openptv_c_lib
    openptv_py_wrapper

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,32 +1,3 @@
 #!/bin/bash
 
-set -ev
-CWD="$(pwd)"
-echo $CWD
-
-cd ./openptv/liboptv
-mkdir _build && cd _build
-cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
-make
-# make verify # check_track fails and I need to continue
-make install
-# cp ./src/liboptv.dylib ../../py_bind
-
-cd ../../py_bind
-# python setup.py build_ext -I../liboptv/include -L../liboptv/_build/src 
-python setup.py install --prefix=$PREFIX
-
-# export PATH=$PATH:/usr/local/lib
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/lib
-# echo $LD_LIBRARY_PATH
-# echo $PATH
-# python -c "import sys; print sys.path"
-
-cd test
-nosetests -v
-
-echo $PWD
-cd $CWD
-
-python setup.py install --prefix=$PREFIX
+$PYTHON setup.py install
