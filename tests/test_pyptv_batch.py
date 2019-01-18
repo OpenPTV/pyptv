417a6be508cfd0dc4956faa4fafd7b418532f8dd
Alex Liberzon <alex.liberzon@gmail.com>
2019-01-18 19:36:38 +0200

    pyptv_batch test has to run from outside of tests

--- tests/test_pyptv_batch.py
+++ tests/test_pyptv_batch.py
@@ -1,4 +1,4 @@
 from pyptv import pyptv_batch
 
 def test_pyptv_batch():
-    assert pyptv_batch.main('./test_cavity', 10000, 10004) is None
+    assert pyptv_batch.main('tests/test_cavity', 10000, 10004) is None
